3217c9d5072ffc8fae2e24037ee7c243635c881f
Benjamin Beckwith <benjamin@kolvanta.com>
2020-10-03 13:28:53 -0600

    Removed 5 modules & added 1 module

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withImages = require('next-images');
 
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-    optimizeImagesInDev: true
-  }],
-  {
-    env: {
-      MONGO_URI: 'mongodb://ben:admin1234@ds139037.mlab.com:39037/shopping-cart-template',
-      secret: ''
-    }
+module.exports = withImages({
+  env: {
+    MONGO_URI: 'mongodb://ben:admin1234@ds139037.mlab.com:39037/shopping-cart-template',
+    secret: ''
   }
-]);
+})
