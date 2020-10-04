f6f98174d0cbc13a2f286cfa97641526dfea1c5e
Benjamin Beckwith <benjamin@kolvanta.com>
2020-10-04 11:18:12 -0600

    Deleted react-native specific statement

--- utils/firebaseConnect.js
+++ utils/firebaseConnect.js
@@ -13,9 +13,6 @@ const firebaseConfig = {
   messagingSenderId: '547344388751',
 };
 
-// Firebase sets some timeers for a long period, which will trigger some warnings. Let's turn that off for this example
-console.disableYellowBox = true;
-
 try {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
